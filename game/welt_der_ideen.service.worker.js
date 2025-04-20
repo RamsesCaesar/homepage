849d2689968c837e7a6e97ef481898847aaf6ae6
Paul Heinrich Bayer <paul.heinrich.bayer.1@hu-berlin.de>
2025-04-21 00:11:32 +0200

    update game

--- game/welt_der_ideen.service.worker.js
+++ game/welt_der_ideen.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1745184804|4130060088';
+const CACHE_VERSION = '1745187027|77162888';
 /** @type {string} */
 const CACHE_PREFIX = 'Mind Map World-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
